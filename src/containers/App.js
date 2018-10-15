ac80ab29f14159f59a768a7a62e5b75acb22d884
MARTIN Cédric <cedricmart8@gmail.com>
2018-10-15 14:39:36 +0200

    test modification + ajout du package-lock dans le ignore

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,16 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/ClementAucoin/orb-web"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Projet MicroService - ORB 
           </a>
         </header>
       </div>
